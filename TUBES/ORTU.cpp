76628b22660f855aa828ebfeafbdd9d0be20c334
luqmanhadisudiana <62825537+luqmanhadisudiana@users.noreply.github.com>
2020-03-29 15:30:57 +0700

    Update ORTU.cpp

--- TUBES/ORTU.cpp
+++ TUBES/ORTU.cpp
@@ -1,2 +1,87 @@
 #include <iostream>
 #include "h.ORTU.h"
+
+void createList(listAnak &L){
+    first(L) = NULL;
+    last(L) = NULL;
+}
+bool isEmpty(listAnak L){
+    if(first(L) == NULL) {
+        return true;
+    } else {
+        return false;
+    }
+}
+void createElementAnak(address &P){
+    string nama, tempattinggal, tmpt;
+    int tl, tahunlahir;
+
+    P = new elmlist_anak;
+    next(P) = NULL;
+    prev(P) = NULL;
+
+    cout<<"================================= MASUKAN DATA ANAK =========================================="<<endl;
+    cout<<endl;
+
+    cout<<"NAMA ANAK : ";
+    cin>>nama;
+    info(P).nama = nama;
+    cout<<endl;
+
+    cout<<"TEMPAT TANGGAL LAHIR : ";
+    cin>>tmpt;
+    info(P).tmpt = tmpt;
+    cout<< " , ";
+    cin>>tl;
+    info(P).tl = tl;
+    cout<<endl;
+
+    cout<<"TAHUN LAHIR : ";
+    cin>>tahunlahir;
+    cout<<endl;
+    cout<<"UMUR : ";
+    info(P).tahunlahir = 2020 - tahunlahir;
+    cout<<endl;
+
+    cout<<"TEMPAT TINGGAL ANAK : ";
+    cin>>tempattinggal;
+    info(P).tempattinggal = tempattinggal;
+
+}
+void tambahDataAnak(listAnak L, address P){
+    if (isEmpty(L) == true){
+        cout<<"DATA KOSONG"<<endl;
+    }else{
+        next(P) = first(L);
+        prev(first(L)) = P;
+        first(L) = P;
+    }
+}
+void hapusDataAnakTertentu(listAnak &L, address &Prec, address &P){
+    if(Prec == NULL){
+        delete P;
+    }else if(first(L) == NULL){
+        first(L) = P;
+        last(L) = P;
+    }else if(Prec == last(L)){
+        next(Prec) = P;
+        prev(P) = Prec;
+        last(L) = P;
+    }else{
+        next(P) = next(Prec);
+        prev(next(Prec)) = P;
+        next(Prec) = P;
+        prev(P) = Prec;
+    }
+}
+void tampilkanDataAnak(listAnak L){
+    address P = first(L);
+    while (P != NULL){
+        cout<<info(P).nama<<" ";
+        cout<<info(P).tmpt<<" ";
+        cout<<info(P).tl<<" ";
+        cout<<info(P).tahunlahir<<" ";
+        cout<<info(P).tempattinggal<<" ";
+        P = next(P);
+    }
+}

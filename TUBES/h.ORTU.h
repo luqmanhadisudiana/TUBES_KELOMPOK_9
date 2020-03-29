60f1599635117a40d7c796a54adba3945e644056
luqmanhadisudiana <62825537+luqmanhadisudiana@users.noreply.github.com>
2020-03-29 14:27:44 +0700

    Update h.ORTU.h

--- TUBES/h.ORTU.h
+++ TUBES/h.ORTU.h
@@ -25,6 +25,7 @@ struct listAnak{
 
 void createList(listAnak &L);
 void isEmpty(listAnak L);
+void createElementAnak(address &P);
 void tambahDataAnak(listAnak L, address P);
 void hapusDataAnakTertentu(listAnak &L, address Prec, address &P);
 void tampilkanDataAnak(listAnak L);

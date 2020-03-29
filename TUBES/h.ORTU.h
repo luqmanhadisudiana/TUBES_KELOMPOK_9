40c259f45134f462c649adc2b0484bc26aa575a5
luqmanhadisudiana <62825537+luqmanhadisudiana@users.noreply.github.com>
2020-03-29 13:19:57 +0700

    Update h.ORTU.h

--- TUBES/h.ORTU.h
+++ TUBES/h.ORTU.h
@@ -1,5 +1,33 @@
 #ifndef H_ORTU_H_INCLUDED
 #define H_ORTU_H_INCLUDED
+#define next(P) P->next
+#define prev(P) P->prev
+#define first(L) L.first
+#define last(L) L.last
+
+typedef struct elmlist_anak*address;
+struct anak{
+    string nama;
+    int ttl;
+    int tahunlahir;
+    string tempattinggal;
+};
+struct elmlist_anak{
+    anak info;
+    next address;
+    prev address;
+};
+
+struct listAnak{
+    first address;
+    last address;
+};
+
+void createList(listAnak &L);
+void isEmpty(listAnak L);
+void tambahDataAnak(listAnak L, address P);
+void hapusDataAnakTertentu(listAnak &L, address Prec, address &P);
+void tampilkanDataAnak(listAnak L);
 
 
 

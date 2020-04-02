7bdbaa85fe9866d0ef55d02f36ccd9eaa8a97d09
luqmanhadisudiana <62825537+luqmanhadisudiana@users.noreply.github.com>
2020-04-02 16:08:03 +0700

    Update h.ORTU.h

--- TUBES/h.ORTU.h
+++ TUBES/h.ORTU.h
@@ -5,34 +5,36 @@
 #define info(P) P->info
 #define first(L) L.first
 #define last(L) L.last
+#include "h.RELASI.h"
 
 using namespace std;
 
-typedef struct elmlist_anak*address;
+typedef struct elmlist_anak*address_ORTU;
 struct anak{
     string nama;
     string tmpt;
     int tl;
     int tahunlahir;
+    string bln;
     string tempattinggal;
 };
 struct elmlist_anak{
     anak info;
-    address next;
-    address prev;
+    address_ORTU next;
+    address_ORTU prev;
 };
 
 struct listAnak{
-    address first;
-    address last;
+    address_ORTU first;
+    address_ORTU last;
 };
 
 void createList(listAnak &L);
 bool isEmpty(listAnak L);
-void createElementAnak(address &P);
-void tambahDataAnak(listAnak &L, address P);
-address findData(listAnak L, string x);
-void hapusDataAnakTertentu(listAnak &L, address Prec, address &P);
+void createElementAnak(address_ORTU &P);
+void tambahDataAnak(listAnak &L, address_ORTU P);
+address_ORTU findData(listAnak L, string x);
+void hapusDataAnakTertentu(listAnak &L, address_ORTU Prec, address_ORTU &P);
 void tampilkanDataAnak(listAnak L);
 
 #endif // H_ORTU_H_INCLUDED

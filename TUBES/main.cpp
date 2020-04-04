2fb6e308e8168e905050af6362d5a925736d4102
andyano123gifaro <62830729+andyano123gifaro@users.noreply.github.com>
2020-04-04 20:16:03 +0700

    Update main.cpp

--- TUBES/main.cpp
+++ TUBES/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include "h.RELASI.h"
+#include "h.ORTU.h"
+#include "h.AMER.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    listOrtu O;
+    listAnak A;
+    listrelasi R;
+    address_AMER AA;
+    address_ORTU AO;
+    address_RELASI AR;
+
+    createList(O);
+    createList(A);
+    createList(R);
+
+    AO = alokasi("BUDI");
+    tambahDataAnak(A, AO);
+    AO = alokasi("IMAN");
+    tambahDataAnak(A, AO);
+    AO = alokasi("ASEP");
+    tambahDataAnak(A, AO);
+    AO = alokasi("UJANG");
+    tambahDataAnak(A, AO);
+
+    cout<<"================================= LIST ANAK =========================================="<<endl;
+    createElementAnak(AO);
+    tampilkanDataAnak(A);
 }

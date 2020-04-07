11a5f48cdafe4a5293f96232ab2447998ddbb073
luqmanhadisudiana <62825537+luqmanhadisudiana@users.noreply.github.com>
2020-04-08 00:24:43 +0700

    Update h.RELASI.h

--- TUBES/h.RELASI.h
+++ TUBES/h.RELASI.h
@@ -2,11 +2,12 @@
 #define H_RELASI_H_INCLUDED
 #define next(P) P->next
 #define prev(P) P->prev
-#define info(P) P->info
+#define infoAmer(P) P->infoAmer
 #define first(L) L.first
 
 #include "h.RELASI.h"
 #include "h.AMER.h"
+#include "h.ORTU.h"
 #include <iostream>
 
 using namespace std;
@@ -15,20 +16,23 @@ typedef struct elmlist_relasi*address_RELASI;
 struct elmlist_relasi{
     address_RELASI next;
     address_AMER info;
+    address_ORTU info1;
 };
 
 struct listrelasi{
     address_RELASI first;
 };
 
-void CreateList(listrelasi &L);
+void createList(listrelasi &L);
 bool isEmpty(listrelasi L);
 address_RELASI Alokasi(address_AMER C);
+void inputRelasi(listrelasi &LR, listOrtu LO, listAnak LA);
 void InsertFirst(listrelasi &L, address_RELASI P);
 void InsertLast(listrelasi &L, address_RELASI P);
 void InsertAfter(address_RELASI Prec, address_RELASI P);
 void DeleteFirst(listrelasi &L, address_RELASI &P);
-address_RELASI findData(listrelasi L, string x);
+address_RELASI findDataRelasiNamaAnak(listrelasi L, listAnak LA , string x);
+address_RELASI findDataRelasiNamaOrtu(listrelasi L, listOrtu LO , string x);
 void DeleteAfter(address_RELASI Prec, address_RELASI &P);
 void PrintInfo(listrelasi L);
 

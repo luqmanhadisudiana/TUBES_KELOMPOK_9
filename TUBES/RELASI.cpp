751da5aecce7c636043d08ad0652fa7df72677d0
luqmanhadisudiana <62825537+luqmanhadisudiana@users.noreply.github.com>
2020-04-08 00:24:16 +0700

    Update RELASI.cpp

--- TUBES/RELASI.cpp
+++ TUBES/RELASI.cpp
@@ -2,15 +2,44 @@
 #include "h.RELASI.h"
 
 
-void CreateList(listrelasi &L) {
+void createList(listrelasi &L) {
     first(L) = NULL;
 }
 
-address_RELASI Alokasi(address_AMER C) {
+address_RELASI Alokasi(address_AMER ortu, address_ORTU anak) {
     address_RELASI P = new elmlist_relasi;
-    info(P) = C;
+    info(P) = ortu;
     next(P) = NULL;
+    info1(P) = anak;
     return P;
+}
+void inputRelasi(listrelasi &LR, listOrtu LO, listAnak LA){
+    address_ORTU P;
+    address_AMER Q;
+    address_RELASI R;
+    string namaortu;
+    string namaanak;
+
+    tampilkanDataOrtu(LO);
+    cout<<"MASUKAN NAMA ORANG TUA : ";
+    cin>>namaortu;
+    Q = findDataOrtu(LO, namaortu);
+    tampilkanDataOrtu(LO);
+    if( Q != NULL){
+        cout<<endl;
+
+        tampilkanDataAnak(LA);
+
+        if(P != NULL){
+            P = findDataAnak(LA, namaanak);
+            cout<<endl;
+            cout<<"SAYANGILAH KELUARGA DENGAN #DIRUMAHAJA!!!"<<endl;
+        }else{
+            cout<<"ANDA CARI KELUARGA SIAPA?"<<endl;
+        }
+    }else{
+        cout <<"ANDA SALAH KELUARGA!!!" << endl;
+    }
 }
 
 void InsertFirst(listrelasi &L, address_RELASI P) {
